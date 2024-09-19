47b34f9dd166ba5b780668cf3467ea019301157f
chalvnrlv <146017096+chalvnrlv@users.noreply.github.com>
2024-09-19 16:01:22 +0700

    feat: Blog UI Articles

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,12 +1,22 @@
 <x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>    
-    <article>
-        <h3>IPSUM DOLOR</h3>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae laboriosam dolorem temporibus exercitationem cumque unde totam illo minima veritatis quibusdam laborum error alias, quam neque amet sit iste fuga corrupti!</p>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl"> This is Blog! </h3>
+    
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">IPSUM DOLOR</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Chalvin Reza</a> | 17 September 2024
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptatibus velit illo explicabo! Unde nobis vel nam. Saepe itaque aperiam ipsum ab est, voluptas rerum sed at delectus unde odio.</p>
+        <a href="" class="font-medium text-blue-500">Read more &raquo;</a>
     </article>
-    <article>
-        <h3>IPSUM DOLOR</h3>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae laboriosam dolorem temporibus exercitationem cumque unde totam illo minima veritatis quibusdam laborum error alias, quam neque amet sit iste fuga corrupti!</p>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">IPSUM DOLOR</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Chalvin Reza</a> | 18 September 2024
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptatibus velit illo explicabo! Unde nobis vel nam. Saepe itaque aperiam ipsum ab est, voluptas rerum sed at delectus unde odio.</p>
+        <a href="" class="font-medium text-blue-500">Read more &raquo;</a>
     </article>
 
 </x-layout>
