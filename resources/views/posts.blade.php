e5ae20f01619b75b057994c430ae58c2007395bb
chalvnrlv <146017096+chalvnrlv@users.noreply.github.com>
2024-09-26 04:41:01 +0700

    feat: Database Model Posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,16 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    @foreach ($posts as $posts)
+    @foreach ($posts as $post)
         <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $posts['title'] }}</h2>
+          <a href="/posts/{{ $post['slug'] }}" class="hover:underline">  
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+          </a>  
             <div class="text-base text-gray-500">
-                <a href="#">{{ $posts['author'] }}</a> | 17 September 2024
+                <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->format('j F Y') }}
             </div>
-            <p class="my-4 font-light">{{ Str::limit($posts['body'], 75) }}</p>
-            <a href="" class="font-medium text-blue-500">Read more &raquo;</a>
+            <p class="my-4 font-light">{{ Str::limit($post['body'], 75) }}</p>
+            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
         </article>
     @endforeach
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,24 +13,13 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'title' => 'LOREM IPSUM 1',
-            'author' => 'Chalvin Reza',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptatibus velit illo explicabo! Unde nobis vel nam. Saepe itaque aperiam ipsum ab est, voluptas rerum sed at delectus unde odio.'
-        ],
-        [
-            'title' => 'LOREM IPSUM 2',
-            'author' => 'Reza Farrel',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, maiores sit minima esse reprehenderit sed excepturi voluptate voluptatibus, aspernatur nemo delectus assumenda molestias deserunt labore praesentium provident impedit ratione error!'
-        ],
-        [
-            'title' => 'LOREM IPSUM 3',
-            'author' => 'Chalvin Farrel',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, quidem distinctio perspiciatis fugiat aperiam eligendi. Perspiciatis ea expedita eius inventore in tempore architecto, nostrum error at animi ex, hic recusandae.'
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
+
+Route::get('/posts/{post:slug}', function(Post $post) {
+
+    return view('post', ['title' => 'Article', 'post' => $post]);
+}); 
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contacts'], ['nama' => 'Chalvin Reza']);
