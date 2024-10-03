b069a3d4ff5109a15c0492fe5fb1e44e73f91056
chalvnrlv <146017096+chalvnrlv@users.noreply.github.com>
2024-10-04 01:49:32 +0700

    refactor: N+1 Problems

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    //$posts = Post::with(['author','category'])->latest()->get();
+    $posts = Post::latest()->get();
+
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function(Post $post) {
@@ -28,11 +31,13 @@ Route::get('/contact', function () {
 });
 
 Route::get('/authors/{user:username}', function(User $user) {
+    //$posts = $user->posts->load('category','author');
 
     return view('posts', ['title' => 'Found '.count($user->posts).' Articles by '. $user->name , 'posts' => $user->posts]);
 }); 
 
 Route::get('/categories/{category:slug}', function(Category $category) {
+    //$posts = $category->posts->load('category','author');
 
     return view('posts', ['title' => 'Found '.count($category->posts).' Articles in : '. $category->name , 'posts' => $category->posts]);
 }); 
